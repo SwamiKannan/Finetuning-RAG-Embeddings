4b404836ef27e57fe8a9dffe0f923e7108c4abf5
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-02-27 01:15:28 +0530

    Update process_data.py
    Updated code since it was not correct where train-test split had to be done:
    1. self.setup_data() now has df as an argument (to be updated later)
    Simplified paths
    write_dict() now returns train and test paths so that we can use them for finetuning

--- src/process_data.py
+++ src/process_data.py
@@ -23,15 +23,16 @@ class HFJSONCreator(JSONAbstractCreator):
     def __init__(self, dataset_name, transform_data=None, split='train',
                  test_ratio=0, to_disk=True):
         self.dataset_name = dataset_name
+        self.save_data_path = os.path.join('data', self.dataset_name.replace('/', "_"))
         self.split = split
         self.df = None
         self.test_ratio = test_ratio
         self.to_disk = to_disk
         self.transform_data = transform_data
-        self.path = '..//processed_data//templated_json'
-        self.data_path = '..//datasets'
+        self.input_path = os.path.join('..', self.save_data_path, 'input_json')
+        self.data_path = os.path.join('..', self.save_data_path, 'embeddings')
         try:
-            os.makedirs(self.path)
+            os.makedirs(self.input_path)
         except Exception:
             pass
         try:
@@ -48,42 +49,49 @@ class HFJSONCreator(JSONAbstractCreator):
         self.df = self.data.to_pandas()
 
     def split_data(self):
-        self.train, self.test = train_test_split(self.df, self.test_ratio)
+        train, test = train_test_split(self.df, test_size=self.test_ratio)
+        return train, test
 
     def create_dict(self, df):
-        self.setup_data()
+        self.setup_data(df)
         dict_qa = {i: {'Question': q, 'Reference': a} for i, (q, a) in
-                   enumerate(zip(list(df['question']), list(df['answer'])))
+                   enumerate(zip(list(df['question']),
+                                 list(df['answer'])))
                    if a != ""}
         return dict_qa
 
-    def setup_data(self):
+    def setup_data(self,df):
         if self.transform_data:
-            self.transform_data(self.df)
+            self.transform_data(df)
+            assert 'question' in df.columns
+            assert 'answer' in df.columns
+            assert isinstance(df.iloc[0]['question'], str)
 
     def create_all_dicts(self):
         if self.test_ratio == 0:
             dict_qa_train = self.create_dict(self.df)
             dict_qa_test = None
         else:
-            self.split_data()
-            dict_qa_train = self.create_dict(self.train)
-            dict_qa_test = self.create_dict(self.test)
+            train, test = self.split_data()
+            dict_qa_train = self.create_dict(train)
+            dict_qa_test = self.create_dict(test)
         return dict_qa_train, dict_qa_test
 
     def write_dict(self):
-        if self.test_ratio == 0:
-            output_filename = self.dataset_name.replace('/', '')+'_'+self.split
-            qa_train, qa_test = self.create_all_dicts()
-            with open(os.path.join(self.path, output_filename+'.json'), 'w',
-                      encoding='utf-8') as f:
-                json.dump(qa_train, f)
-            if qa_test:
-                with open(os.path.join(self.path, output_filename.replace(
-                    self.split, ''+'_test.json')),
-                          'w', encoding='utf-8') as f:
-                    json.dump(qa_test, f)
-        print(f'{output_filename}.json file written in {self.path}')
+        output_filename = self.dataset_name.replace('/', '')+'_'+self.split
+        qa_train, qa_test = self.create_all_dicts()
+        train_path = os.path.join(self.input_path, output_filename+'.json')
+        test_path = os.path.join(self.input_path, 
+                                   output_filename.replace(self.split, '') +
+                                   '_test.json')        
+        with open(os.path.join(train_path), 'w',
+                  encoding='utf-8') as f:
+            json.dump(qa_train, f)
+        if qa_test:
+            with open(os.path.join(test_path), 'w', encoding='utf-8') as f:
+                json.dump(qa_test, f)
+        print(f'{output_filename}.json file written in {self.input_path}')
+        return train_path, test_path, self.save_data_path
 
 
 class LocalJSONCreator(HFJSONCreator):
