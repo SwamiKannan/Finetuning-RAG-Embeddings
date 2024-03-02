fbff5a4ab7739dbe13ac03e7851b2329d4565e75
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-03-02 13:11:35 +0530

    Create dataset_setup.py
    Created a class just to consolidate and create all requisite variables and paths for any dataset

---
+++ src/dataset_setup.py
@@ -0,0 +1,29 @@
+import os
+
+
+class Data_setup():
+    def __init__(self, main_path, model_outputs=[], corpus_count=5):
+        self.main_path = main_path
+        self.EMBEDDINGS_PATH = 'embeddings'
+        self.DATA_PATH = 'data'
+        self.LOG_PATH = ''
+        self.FINAL_JSON = "final_json.json"
+        self.CORPUS_COUNT = corpus_count
+        self.data_path = self.make_dirs(self.DATA_PATH)
+        self.embeddings_path = self.make_dirs(folder_name=self.EMBEDDINGS_PATH, path=self.data_path)
+        self.log_path = self.make_dirs(folder_name=self.LOG_PATH, path=self.data_path)
+        self.final_json = os.path.join(self.data_path, self.FINAL_JSON)
+        self.corpus_count = self.CORPUS_COUNT
+        self.models = {}
+        self.models_path = {}
+        for i in range(len(model_outputs)):
+            self.models_path[i] = self.make_dirs(folder_name=model_outputs[i], path=self.embeddings_path)
+
+    def make_dirs(self, folder_name, path=None):
+        if path:
+            temp_path = os.path.join(path, folder_name)
+        else:
+            temp_path = os.path.join(self.main_path, folder_name)
+        if not os.path.exists(temp_path):
+            os.makedirs(temp_path)
+        return temp_path
