4f62e1ee7b1a17f57472f8cb0992a231c4d8d109
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-03-02 13:14:44 +0530

    Update finetuner.py
    removed all the variables from this code and refactored into separate file. Moved to if __name__=="__main__" code to run this independently, if required

--- src/finetuner.py
+++ src/finetuner.py
@@ -1,24 +1,5 @@
 import os
-from finetune_utils import finetune, create_json_input
-
-EMBEDDINGS_PATH = '..//embeddings'
-MODEL_ID = 'physics_2'
-MODEL_INPUT_PATH = os.path.join(EMBEDDINGS_PATH, MODEL_ID)
-MODEL_OUTPUT = 'physics_3'
-MODEL_OUTPUT_PATH = os.path.join(EMBEDDINGS_PATH, MODEL_OUTPUT)
-
-BASE_JSON_PATH = "..//processed_data//base_data_json"
-TEMPLATED_JSON_PATH = "..//processed_data//templated_json"
-
-INPUT_FILE = "camelai//camel-ai_physics_train.json"
-if 'camelai' not in os.listdir(TEMPLATED_JSON_PATH):
-    os.makedirs(os.path.join(TEMPLATED_JSON_PATH, 'camelAI'))
-OUTPUT_JSON_FILE = 'camelai//camel_train.json'
-
-BASE_INPUT_PATH = os.path.join(BASE_JSON_PATH, INPUT_FILE)
-TEMPLATED_PATH = os.path.join(TEMPLATED_JSON_PATH, OUTPUT_JSON_FILE)
-
-NUMBER_OF_CORPUS_ANSWERS = 10
+from finetune_utils import finetune, create_json_input, finetune2
 
 
 def final_finetune(input_path, output_path, corpus_count, model_input,
@@ -28,7 +9,35 @@ def final_finetune(input_path, output_path, corpus_count, model_input,
     finetune(output_path, model_input, model_output)
 
 
+def final_finetune2(input_path, output_path, corpus_count, model_input,
+                    model_output, w_path):
+    print('Creating JSON input for finetuning')
+    create_json_input(input_path, output_path,
+                      corpus_count)
+    print('JSON file created.')
+    finetune2(output_path, model_input, model_output, w_path=w_path)
+    print(f'Finetuning completed. Model stored at {model_output}')
+
+
 if __name__ == "__main__":
+    EMBEDDINGS_PATH = '..//embeddings'
+    MODEL_ID = 'physics_2'
+    MODEL_INPUT_PATH = os.path.join(EMBEDDINGS_PATH, MODEL_ID)
+    MODEL_OUTPUT = 'physics_3'
+    MODEL_OUTPUT_PATH = os.path.join(EMBEDDINGS_PATH, MODEL_OUTPUT)
+
+    BASE_JSON_PATH = "..//processed_data//base_data_json"
+    TEMPLATED_JSON_PATH = "..//processed_data//templated_json"
+
+    INPUT_FILE = "camelai//camel-ai_physics_train.json"
+    if 'camelai' not in os.listdir(TEMPLATED_JSON_PATH):
+        os.makedirs(os.path.join(TEMPLATED_JSON_PATH, 'camelAI'))
+    OUTPUT_JSON_FILE = 'camelai//camel_train.json'
+
+    BASE_INPUT_PATH = os.path.join(BASE_JSON_PATH, INPUT_FILE)
+    TEMPLATED_PATH = os.path.join(TEMPLATED_JSON_PATH, OUTPUT_JSON_FILE)
+
+    NUMBER_OF_CORPUS_ANSWERS = 10
     create_json_input(BASE_INPUT_PATH, TEMPLATED_PATH,
                       NUMBER_OF_CORPUS_ANSWERS)
     finetune(TEMPLATED_PATH, MODEL_INPUT_PATH, MODEL_OUTPUT_PATH,)
