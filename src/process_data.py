9b188641266bdf9a6643f8bf61fca02025827c64
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-03-02 13:19:38 +0530

    Update process_data.py
    Removed all main dir creation activities from here to out into dataset_setup to reduce confusion. Only keeping dir creation for files created in this current code,
    Code formatting

--- src/process_data.py
+++ src/process_data.py
@@ -20,32 +20,35 @@ class JSONAbstractCreator(ABC):
 
 
 class HFJSONCreator(JSONAbstractCreator):
-    def __init__(self, dataset_name, transform_data=None, split='train',
-                 test_ratio=0, to_disk=True):
+    def __init__(self, dataset_name, data_path, transform_data=None, split='train',
+                 test_ratio=0, to_disk=True, ):
         self.dataset_name = dataset_name
-        self.save_data_path = os.path.join('data', self.dataset_name.replace('/', "_"))
+        self.download_filename = dataset_name.replace('/', '')
+        self.data_path = data_path
+        self.save_download = 'dataset'
         self.split = split
         self.df = None
         self.test_ratio = test_ratio
         self.to_disk = to_disk
         self.transform_data = transform_data
-        self.input_path = os.path.join('..', self.save_data_path, 'input_json')
-        self.data_path = os.path.join('..', self.save_data_path, 'embeddings')
-        try:
-            os.makedirs(self.input_path)
-        except Exception:
-            pass
-        try:
-            os.mkdir(self.data_path)
-        except Exception:
-            pass
+        self.data_path = data_path
+        self.INTERIM_JSON_FILE_NAME = 'interim_json'
+        self.interim_json_path = os.path.join(self.data_path, self.INTERIM_JSON_FILE_NAME)
+        self.save_download_path = os.path.join(self.data_path, self.save_download)
+        if not os.path.exists(self.save_download_path):
+            os.makedirs(self.save_download_path)
+
         self.load_data()
 
     def load_data(self):
+        print('Downloading data.....')
         self.data = load_dataset(self.dataset_name, split=self.split)
         if self.to_disk:
+            print('Data downloading... saving data')
             self.filename = self.dataset_name+'_'+self.split
-            self.data.save_to_disk(os.path.join(self.data_path, self.filename))
+            self.data.save_to_disk(os.path.join(self.save_download_path, self.download_filename))
+        else:
+            print('Data downloaded...')
         self.df = self.data.to_pandas()
 
     def split_data(self):
@@ -60,7 +63,7 @@ class HFJSONCreator(JSONAbstractCreator):
                    if a != ""}
         return dict_qa
 
-    def setup_data(self,df):
+    def setup_data(self, df):
         if self.transform_data:
             self.transform_data(df)
             assert 'question' in df.columns
@@ -78,20 +81,20 @@ class HFJSONCreator(JSONAbstractCreator):
         return dict_qa_train, dict_qa_test
 
     def write_dict(self):
-        output_filename = self.dataset_name.replace('/', '')+'_'+self.split
+        output_filename = self.INTERIM_JSON_FILE_NAME+'_'+self.split
         qa_train, qa_test = self.create_all_dicts()
-        train_path = os.path.join(self.input_path, output_filename+'.json')
-        test_path = os.path.join(self.input_path, 
-                                   output_filename.replace(self.split, '') +
-                                   '_test.json')        
+        train_path = os.path.join(self.data_path, output_filename+'.json')
+        test_path = os.path.join(self.data_path,
+                                 output_filename.replace(self.split, '') +
+                                 '_test.json')
         with open(os.path.join(train_path), 'w',
                   encoding='utf-8') as f:
             json.dump(qa_train, f)
         if qa_test:
             with open(os.path.join(test_path), 'w', encoding='utf-8') as f:
                 json.dump(qa_test, f)
-        print(f'{output_filename}.json file written in {self.input_path}')
-        return train_path, test_path, self.save_data_path
+        print(f'Interim json: {output_filename}.json file written in {self.data_path}')
+        return train_path, test_path, self.data_path
 
 
 class LocalJSONCreator(HFJSONCreator):
