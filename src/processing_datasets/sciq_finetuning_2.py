8d02562a4a1a45b9bd2bbe04f6aeffa21192a6bb
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-03-05 10:29:04 +0530

    Update sciq_finetuning_2.py
    Code updated to segregate static and data-specific parameters in this file rather than push path name creation in the imported class and functions
    Added functionality to process and store test and eval sets

--- src/processing_datasets/sciq_finetuning_2.py
+++ src/processing_datasets/sciq_finetuning_2.py
@@ -4,34 +4,52 @@ import os
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from process_data import HFJSONCreator
-from finetuner import final_finetune
+# from finetuner import final_finetune2
+from dataset_setup import Data_setup
+from finetune_utils import create_json_input, final_finetune2
 
-MODEL_ID1 = "embeddings_camelai"
+
+MAIN_PATH1 = '..//sciq_physics'
+MAIN_PATH2 = '..//sciq_physics2'
+
+MODEL_ID1 = "..//camelai_physics//data//embeddings//embeddings_camelai"
 MODEL_ID2 = "BAAI/bge-small-en-v1.5"
-DATASET = 'camel-ai/physics'
-FILE_PATH = 'final_json.json'
+DATASET = 'allenai/sciq'
 
 CORPUS_COUNT = 5
 MODEL_OUTPUT1 = 'camelai_sciq'
 MODEL_OUTPUT2 = 'embeddings_sciq'
+TEST_RATIO = 0
 
-
+SPLITS = ['train', 'validate','test']
 def transform_df(df):
-    df['answer'] = df['topic;'] + ' ' + df['sub_topic'] + ' '+df['message_2']
-    df['question'] = df['message_1']
+    df['answer'] = df['correct_answer'] + ' ' + df['support']
 
 
-camelai = HFJSONCreator(dataset_name=DATASET, transform_data=transform_df,
-                        test_ratio=0.2, to_disk=True)
+model_dict = {MODEL_ID1: (MODEL_OUTPUT1, MAIN_PATH1), MODEL_ID2: (MODEL_OUTPUT2, MAIN_PATH2)}
 
-camelai.create_all_dicts()
-train_path, test_path, main_dir = camelai.write_dict()
-
-for model_id, model_output in zip([MODEL_ID1, MODEL_ID2],
-                                  [MODEL_OUTPUT1, MODEL_OUTPUT2]):
-    final_finetune(train_path, os.path.join('..', main_dir, FILE_PATH),
-                   CORPUS_COUNT, model_id, model_output)
-
+for k, v in model_dict.items():
+    output = v[0]
+    main_path=v[1]
+    print(f'Running {k}')
+    for split in SPLITS:
+        data_sciq = Data_setup(main_path, model_outputs=output, split=split)
+        sciq = HFJSONCreator(dataset_name=DATASET, transform_data=transform_df,
+                         test_ratio=TEST_RATIO, to_disk=True, data_path=data_sciq.data_path)
+        sciq.create_all_dicts()
+        train_path, test_path, main_dir = sciq.write_dict()
+        if split == 'train':
+            final_finetune2(input_path=train_path, output_path=data_sciq.final_json,
+                            corpus_count=CORPUS_COUNT, model_input=k,
+                            model_output=data_sciq.model_path, w_path=data_sciq.log_path)
+            if test_path:
+                create_json_input(filename=test_path, output_file=data_sciq.final_json[:-4]+'_test.json',
+                                  negative_size=CORPUS_COUNT)
+        
+        else:
+            create_json_input(filename=train_path, output_file=data_sciq.final_json',
+                              negative_size=CORPUS_COUNT)
+        
 
 
 
