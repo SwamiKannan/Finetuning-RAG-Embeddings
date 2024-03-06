b63fcdef8c9d860211c8d30bfe6a5af9837dbcd5
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-03-06 14:42:04 +0530

    Update sciq_finetuning_2.py
    Removed finetuner as an import since all util functions from finetuner moved to finetune_utils,

--- src/finetune_examples/sciq_finetuning_2.py
+++ src/finetune_examples/sciq_finetuning_2.py
@@ -4,10 +4,8 @@ import os
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from process_data import HFJSONCreator
-# from finetuner import final_finetune2
 from dataset_setup import Data_setup
 from finetune_utils import create_json_input, final_finetune2
-
 
 MAIN_PATH1 = '..//sciq_physics'
 MAIN_PATH2 = '..//sciq_physics2'
@@ -21,12 +19,42 @@ MODEL_OUTPUT1 = 'camelai_sciq'
 MODEL_OUTPUT2 = 'embeddings_sciq'
 TEST_RATIO = 0
 
-SPLITS = ['train', 'validate','test']
+SPLITS = ['train', 'validate', 'test']
+EPOCHS = 3
+
 def transform_df(df):
     df['answer'] = df['correct_answer'] + ' ' + df['support']
 
 
 model_dict = {MODEL_ID1: (MODEL_OUTPUT1, MAIN_PATH1), MODEL_ID2: (MODEL_OUTPUT2, MAIN_PATH2)}
+
+for k, v in model_dict.items():
+    output = v[0]
+    main_path = v[1]
+    print(f'Running {k}')
+    for split in SPLITS:
+        data_sciq = Data_setup(main_path, model_outputs=output, split=split)
+        sciq = HFJSONCreator(dataset_name=DATASET, transform_data=transform_df,
+                             test_ratio=TEST_RATIO, to_disk=True,
+                             data_path=data_sciq.data_path)
+        sciq.create_all_dicts()
+        train_path, test_path, main_dir = sciq.write_dict()
+        if split == 'train':
+            final_finetune2(input_path=train_path, output_path=data_sciq.final_json,
+                            corpus_count=CORPUS_COUNT, model_input=k,
+                            model_output=data_sciq.model_path, w_path=data_sciq.log_path,epochs=EPOCHS)
+            if test_path:
+                create_json_input(filename=test_path,
+                                  output_file=data_sciq.final_json.replace('train','test'), # Remove train
+                                  negative_size=CORPUS_COUNT)
+        else:
+            create_json_input(filename=train_path,
+                              output_file=data_sciq.final_json,
+                              negative_size=CORPUS_COUNT)
+
+
+
+'''
 
 for k, v in model_dict.items():
     output = v[0]
@@ -70,7 +98,6 @@ for k, v in model_dict.items():
 
 
 
-'''
 import sys
 import os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
